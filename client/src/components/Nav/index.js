9ca5ca194185350df1ae03030387444f88d0981b
GitDidrik <lindberg.didrik@gmail.com>
2023-09-25 17:07:29 -0700

    homepage sections

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,54 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
 function Nav() {
+  const [MobileMenuActive, setShowMobileMenu] = useState(false);
+
+  // Function to toggle the mobile menu
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!MobileMenuActive);
+  };
+
+ 
 
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
+        <ul className={`flex-row ${MobileMenuActive ? "mobile-menu" : ""}`}>
+          <li className="nav-links" style={{ float: "left", fontWeight: "bold" }}>
+            <Link to="/">Opulence</Link>
           </li>
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px", marginLeft: "10px" }}>
+            <Link to="/orderHistory">Order History</Link>
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            |
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            <a href="/" onClick={() => Auth.logout()}>Logout</a>
           </li>
         </ul>
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
+        <ul className={`flex-row ${MobileMenuActive ? "mobile-menu" : ""}`}>
+          <li className="nav-links" >
+            <Link to="/" style={{ fontWeight: "lighter" }}>Services</Link>
           </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            |
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            <Link to="/signup" style={{ fontWeight: "lighter" }}>About</Link>
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            |
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            <Link to="/login" style={{ fontWeight: "lighter" }}>Testimonials</Link>
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            |
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            <Link to="/signup" style={{ fontWeight: "lighter" }}>Signup</Link>
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            |
+          </li>
+          <li className="nav-links" style={{ float: "right", marginRight: "10px" }}>
+            <Link to="/login" style={{ fontWeight: "lighter" }}>Login</Link>
           </li>
         </ul>
       );
     }
   }
-
   return (
     <header className="flex-row px-1">
       <h1>
-        <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
-        </Link>
+        <Link to="/">Opulence</Link>
       </h1>
-
-      <nav>
+      <div className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+        <div className={`hamburger ${MobileMenuActive ? "active" : ""}`}></div>
+      </div>
+      <nav className={`mobile-menu ${MobileMenuActive ? "active" : ""}`}>
         {showNavigation()}
       </nav>
     </header>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,126 +2,218 @@ import React from "react";
 import HomepageImage from "../assets/Homepage2.jpg"; // Import the background image
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faFlag, faStar } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
+  const imageStyle = {
+    backgroundImage: `url(${HomepageImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    position: "relative",
+    filter: "brightness(90%) blur(3.5px) opacity(0.8)",
+    zIndex: 0,
+  };
+
+
+  const contentStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    
+    
+  };
+
   return (
     <main>
       {/* First Section - Background Image */}
-      <section className="homepage-section">
+      <section >
+        <div style={imageStyle} className="homepage-section"></div>
+        <div style={{ zIndex: 1 }}>
         <div className="container h-100">
-          <div className="row h-100 justify-content-center align-items-center">
-            <div className="col-md-6">
-              {/* Tagline */}
-              <h1 className="text-white">Your Tagline Goes Here</h1>
-              {/* Short Description */}
-              <p className="text-white">
-                A short description of your services or business can go here.
-              </p>
-              {/* Buttons */}
-              <div>
-                <Button variant="dark" className="mr-2">
-                  Book Now!
-                </Button>
-                <Button variant="outline-dark" className="text-white">
-                  Read More
-                </Button>
+          
+          <div className="row h-100">
+            <div className="col-md-12">
+              <div className="content-home"style={contentStyle}>
+                {/* Tagline */}
+                <h1 className="tagline">Sophisticated Cleaning for the Modern Home</h1>
+                {/* Short Description */}
+                <p  className="description">
+                  Welcome to Opulence, your trusted partner for exceptional home cleaning. 
+                  Experience the difference with our premium service. We bring cleanliness, convenience, and peace of mind to your doorstep. Schedule a service today!
+                </p>
+                {/* Buttons */}
+                <div>
+                  <Button variant="dark" className="text-white home-button">
+                    Book Now
+                  </Button>
+                  <Button variant="outline-dark" className="home-button">                    
+                  Learn More
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </section>  {/* Second Section - Cards */}
+      <section className="py-5">
+      <div className="container">
+        <div className="row">
+          {/* Card 1 */}
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-icon">
+                  <FontAwesomeIcon icon={faCheck} />
+                </div>
+                <h5 className="card-title">Efficient Cleaning</h5>
+                <p className="card-text">
+                Efficiently completed, leaving your home fresh, tidy, 
+                and ready for your peace of mind. Your sanctuary, our commitment.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Card 2 */}
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-icon">
+                  <FontAwesomeIcon icon={faFlag} />
+                </div>
+                <h5 className="card-title">Attention to Detail</h5>
+                <p className="card-text">
+                Meticulously flagged areas receive extra care, 
+                ensuring a higher level of cleanliness and organization throughout your space. Every detail, our priority.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Card 3 */}
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-icon">
+                  <FontAwesomeIcon icon={faStar} />
+                </div>
+                <h5 className="card-title">Elite Cleaning Service</h5>
+                <p className="card-text">
+                Discover luxury and convenience with Elite Clean: thorough top-to-bottom cleaning and persinalized treatments for pristine homes.
+                </p>
               </div>
             </div>
           </div>
         </div>
-      </section>      {/* Second Section - Cards */}
-      <section className="py-5">
-        <div className="container">
-          <div className="row">
-            {/* Card 1 */}
-            <div className="col-md-4">
-              <Card>
-                <Card.Body>
-                  <Card.Title>Card 1</Card.Title>
-                  <Card.Text>
-                    Description of Card 1.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-            {/* Card 2 */}
-            <div className="col-md-4">
-              <Card>
-                <Card.Body>
-                  <Card.Title>Card 2</Card.Title>
-                  <Card.Text>
-                    Description of Card 2.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-            {/* Card 3 */}
-            <div className="col-md-4">
-              <Card>
-                <Card.Body>
-                  <Card.Title>Card 3</Card.Title>
-                  <Card.Text>
-                    Description of Card 3.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-          </div>
-        </div>
-      </section>
+      </div>
+    </section>
 
       {/* Third Section - More Cards */}
       <section className="py-5">
-        <div className="container">
+         <div className="container">
           <div className="row">
-            {/* Card 4 */}
-            <div className="col-md-4">
-              <Card>
-                <Card.Body>
-                  <Card.Title>Card 4</Card.Title>
-                  <Card.Text>
-                    Description of Card 4.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-            {/* Card 5 */}
-            <div className="col-md-4">
-              <Card>
-                <Card.Body>
-                  <Card.Title>Card 5</Card.Title>
-                  <Card.Text>
-                    Description of Card 5.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </div>
-            {/* Card 6 */}
-            <div className="col-md-4">
-              <Card>
-                <Card.Body>
-                  <Card.Title>Card 6</Card.Title>
-                  <Card.Text>
-                    Description of Card 6.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
+            <div className="col-md-12 text-center">
+               <h2>Featured Services</h2>
+             </div>
             </div>
           </div>
+  <div className="container">
+    <div className="row">
+      {/* Card 4: Basic */}
+      <div className="col-md-4">
+        <div className="service-card">
+          <h2 className="service-heading">Basic</h2>
+          <h3 className="service-title">$150</h3>
+          <p className="service-description">
+          Our Basic Clean service provides a thorough and essential cleaning of your home.
+          We focus on key areas, ensuring a fresh and tidy living space.
+          </p>
+          <button className="service-button">Book</button>
         </div>
-      </section>
+      </div>
+      {/* Card 5: Premium */}
+      <div className="col-md-4">
+        <div className="service-card">
+          <h2 className="service-heading">Premium</h2>
+          <h3 className="service-title">$225</h3>
+          <p className="service-description">
+          Elevate your cleaning experience with our Premium Clean service.
+          We go the extra mile, tackling more areas and paying attention to details
+          </p>
+          <button className="service-button">Book</button>
+        </div>
+      </div>
+      {/* Card 6: Elite */}
+      <div className="col-md-4">
+        <div className="service-card">
+          <h2 className="service-heading">Elite</h2>
+          <h3 className="service-title">$300</h3>
+          <p className="service-description">
+          For the ultimate in luxury and cleanliness, choose our Elite Clean service. 
+          Our expert team provides a comprehensive top-to-bottom cleaning, 
+          including 
+          </p>
+          <button className="service-button">Book</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
-      {/* Fourth Section - To Be Determined */}
-      <section className="py-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <h2>Fourth Section Content</h2>
-              {/* Add content for the fourth section here */}
-            </div>
+
+<section className="py-5">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12 text-center">
+        <h2>Customer Reviews</h2>
+      </div>
+    </div>
+    <div className="row">
+      {/* Review Card 1 */}
+      <div className="col-md-4">
+        <div className="review-card">
+          <div className="review-avatar">
+            {/* Add an avatar image if desired */}
           </div>
+          <h3 className="review-title">Excellent Service</h3>
+          <p className="review-text">
+            Opulence provided an excellent cleaning service for my home. 
+            They paid attention to every detail, and my house has never looked cleaner.
+          </p>
+          <p className="review-author">- Sophia Martinez</p>
         </div>
-      </section>
+      </div>
+      {/* Review Card 2 */}
+      <div className="col-md-4">
+        <div className="review-card">
+          <div className="review-avatar">
+            {/* Add an avatar image if desired */}
+          </div>
+          <h3 className="review-title">Highly Recommended</h3>
+          <p className="review-text">
+            I'm extremely satisfied with Opulence's cleaning service. 
+            Their attention to detail and professionalism are unmatched.
+          </p>
+          <p className="review-author">- Emily Johnson</p>
+        </div>
+      </div>
+      {/* Review Card 3 */}
+      <div className="col-md-4">
+        <div className="review-card">
+          <div className="review-avatar">
+            {/* Add an avatar image if desired */}
+          </div>
+          <h3 className="review-title">Great Experience</h3>
+          <p className="review-text">
+            Opulence made my home spotless! I'm very impressed with their cleaning service. 
+            I highly recommend them.
+          </p>
+          <p className="review-author">- Michael Anderson</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
     </main>
   );
 }
